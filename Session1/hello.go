4dc114de02f7c8546c4f48a0d553d3a930cb43ac
Szerl0k <mikojoj@gmail.com>
2024-07-25 10:32:14 +0200

    testing messages in mingw

--- Session1/hello.go
+++ Session1/hello.go
@@ -2,15 +2,50 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
+const (
+	spanish = "Spanish"
+	french  = "French"
 
-func Hello(name string) string {
+	frenchHelloPrefix  = "Bonjour, "
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+)
+
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
+
+}
+
+func greetingPrefix(language string) (prefix string) {
+
+	/*
+		if language == spanish {
+			return spanishHelloPrefix + name
+		}
+		if language == french {
+			return frenchHelloPrefix + name
+		}
+		return englishHelloPrefix + name
+	*/
+
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+		break
+	case french:
+		prefix = frenchHelloPrefix
+		break
+	default:
+		prefix = englishHelloPrefix
+		break
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("Chris"))
+	fmt.Println(Hello("Chris", ""))
 }
